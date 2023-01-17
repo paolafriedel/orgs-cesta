2e4d9eb8661bead4443bb50410bc0859a1a84819
paolafriedel <paolabbsf2910@gmail.com>
2023-01-17 17:37:18 -0300

    Tela de carregando

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import mocks from './src/mocks/cesta';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -10,7 +11,7 @@ export default function App() {
   });
 
   if (!fonteCarregada){
-    return <View/>
+    return <AppLoading/>
   }
 
   return (
