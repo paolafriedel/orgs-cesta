35982b4e0e464b0547e88a720f80e460ab88c94b
paolafriedel <paolabbsf2910@gmail.com>
2023-01-20 19:59:27 -0300

    Arrumando a lista de itens

--- src/telas/Cesta/componentes/Itens.js
+++ src/telas/Cesta/componentes/Itens.js
@@ -1,19 +1,50 @@
 import React from "react";
 import Texto from '../../../componentes/Texto';
-import {Image, View} from "react-native";
+import {Image, View, StyleSheet} from "react-native";
 
 export default function Itens({titulo, lista}){
     return <>
-    <Texto>{titulo}</Texto>
+    <Texto style={estilos.titulo}>{titulo}</Texto>
     {lista.map(({nome, imagem}) =>{
-        return <View key={nome}>
-            <Texto>{nome}</Texto>
-            <Image source={imagem}></Image>
+        return <View key={nome} style={estilos.item}>
+            <Image source={imagem} style={estilos.imagem}></Image>
+            <Texto style={estilos.nome}>{nome}</Texto>
+            
         </View>
-        
-
     })}
     </>
-
-
 }
+
+const estilos = StyleSheet.create({
+    titulo:{
+       color:"#464646",
+       fontWeight:"bold",
+       marginTop: 10,
+       marginBottom: 5,
+       fontSize: 20,
+       lineHeight: 32,
+       padding: 16
+    },
+
+    item:{
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ECECEC",
+        paddingVertical: 16,
+        alignItems:"center",
+        
+    },
+    imagem:{
+        width:46,
+        height:46,
+        marginLeft: 16
+
+    },
+    nome:{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color:"#464646"
+
+    }
+})
